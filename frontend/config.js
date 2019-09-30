1988f5e02b63b125fe73fee1d904ffab49817d0b
Jim Orluk <jim.orluk@gmail.com>
2019-09-30 14:01:19 -0400

    updated prodEndpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4402`
-export const prodEndpoint = `https://screens-yoga.jimorluk.com/` //Update this to endpoint when deploying
+export const prodEndpoint = `http://screens-yoga.jimorluk.com/` //Update this to endpoint when deploying
